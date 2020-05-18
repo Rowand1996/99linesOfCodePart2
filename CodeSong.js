e885f0e66a0defe3bed6e456b51d71ff615a9401
Rowan Douglas <rowanandvera@gmail.com>
2020-05-18 13:57:49 -0500

    lab finished !

--- CodeSong.js
+++ CodeSong.js
@@ -1,18 +1,36 @@
-const friends = ["jhon", "bob", "david", "jerry", "morty"];
-for (let i = 0; i < friends.length; i++) {
+const friends = ["john", "vera", "david", "jerry", "morty"];
+const button = document.querySelector("button");
+console.log(button);
+
+button.addEventListener("click", function () {
+
+    friends.forEach(function (friend) {
+        let friendDiv = document.createElement("div");
+        let friendH3 = document.createElement("h3");
+        let singText = document.createElement("p");
+        let friendName = document.createTextNode(friend);
+        friendDiv.className = "friend";
+        friendH3.appendChild(friendName);
+        friendDiv.appendChild(friendH3);
+        document.body.appendChild(friendDiv);
+        for (let j = 99; j > 0; j--) {
+            let word = " lines"
+            if (j === 1) {
+                word = " line"
+            }
+            let lowerNumber = [j] - 1;
+            let word2 = " lines"
+            if (lowerNumber === 1) {
+                word2 = " line"
+            }
+            let friendSing = document.createTextNode([j] + word + " of code in the file " + [j] + word + " of code; " + friend + " strikes one out, clears it all out, " +
+                lowerNumber + word2 + " of code in the file. ");
+            singText.appendChild(friendSing);
+            friendDiv.appendChild(singText);
+        }
+    });
+});
 
 
-    for (let j = 99; j > 0; j--) {
-        let word = " lines"
-        if (j === 1) {
-            word = " line"
-        }
-        let lowerNumber = [j] - 1;
-        let word2 = " lines"
-        if (lowerNumber === 1) {
-            word2 = " line"
-        }
-        console.log(friends[i] + ": " + [j] + word + " of code in the file " + [j] + word + " of code; " + friends[i] + " strikes one out, clears it all out, " +
-            lowerNumber + word2 + " of code in the file");
-    }
-}
+
+
